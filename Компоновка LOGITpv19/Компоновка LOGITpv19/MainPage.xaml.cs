6f4079209af3f4d8d091e0eafc50b1fead55332a
Opilane TTHK <opilane@tthk.ee>
2020-09-04 13:07:53 +0300

    Add project files.

---
+++ Компоновка LOGITpv19/Компоновка LOGITpv19/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Компоновка_LOGITpv19
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
